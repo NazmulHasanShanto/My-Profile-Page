2ea8a441cc378f9c2374c8dbce07de9bb40d0da7
Nazmul Hasan Shanto <129149922+NazmulHasanShanto@users.noreply.github.com>
2024-08-06 16:37:42 +0600

    Add files via upload

---
+++ music/js/music-list.js
@@ -0,0 +1,82 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "আমার সোনার বাংলা আমি তুমাই ভালোবাসি",
+    artist: "জেমস",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "একা দিন ",
+    artist: "রহমান",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " সর‍্যি দীপান্ধিতা",
+    artist: "তারিফ আর সিফাত",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "মা ",
+    artist: "জেমস",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "সোনার_ও_পালঙ্কের_ঘরে",
+    artist: "আতিশ কৃষ্ণকোলি",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "মাধবি_লতা_আমি",
+    artist: "কানিকা",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "এই অবেলায় তোমারি আকাশে",
+    artist: "শিরোনামহীন",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "হাতেও পারে এই দেখা শেষ দেখায়",
+    artist: "জেমস",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "অভিমান",
+    artist: "তানবীরা খাঁন",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "জানি তুমি আসবে না ফিরে ",
+    artist: "শোভন",
+    img: "music-10",
+    src: "music-10"
+  },
+  
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
