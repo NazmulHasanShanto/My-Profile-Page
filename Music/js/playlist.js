8f6ce2110714fc6ba40518a08e419385adaab8e5
Nazmul Hasan Shanto <129149922+NazmulHasanShanto@users.noreply.github.com>
2024-08-06 08:47:51 +0600

    Add files via upload

---
+++ Music/js/playlist.js
@@ -0,0 +1,62 @@
+let songs = [
+  {
+    name: "আমার সোনার বাংলা আমি তুমাই ভালোবাসি",
+    artist: "জেমস",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "একা দিন ",
+    artist: "রহমান",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: " সর‍্যি দীপান্ধিতা",
+    artist: "তারিফ আর সিফাত",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "মা ",
+    artist: "জেমস",
+    img: "img4",
+    audio: "music4"
+  },
+  {
+    name: "সোনার_ও_পালঙ্কের_ঘরে",
+    artist: "আতিশ কৃষ্ণকোলি",
+    img: "img5",
+    audio: "music5"
+  },
+  {
+    name: "মাধবি_লতা_আমি",
+    artist: "কানিকা",
+    img: "img6",
+    audio: "music6"
+  },
+  {
+    name: "এই অবেলায় তোমারি আকাশে",
+    artist: "শিরোনামহীন",
+    img: "img7",
+    audio: "music7"
+  },
+  {
+    name: "হাতেও পারে এই দেখা শেষ দেখায়",
+    artist: "জেমস",
+    img: "img8",
+    audio: "music8"
+  },
+  {
+    name: "অভিমান",
+    artist: "তানবীরা খাঁন",
+    img: "img9",
+    audio: "music9"
+  },
+  {
+    name: "জানি তুমি আসবে না ফিরে ",
+    artist: "শোভন",
+    img: "img10",
+    audio: "music10"
+  }
+]
