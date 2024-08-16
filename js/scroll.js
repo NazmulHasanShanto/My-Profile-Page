adef2c1397153f119d728c6043aee19068f87716
Nazmul Hasan Shanto <129149922+NazmulHasanShanto@users.noreply.github.com>
2024-08-16 08:08:19 +0600

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -15,3 +15,14 @@ function scrollUp(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+// Start of Tawk.to Live Chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/66be9c9d0cca4f8a7a768ccf/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+// End of Tawk.to Live Chat
